adfdd0c7c002fc9e56d386bc512dcff6fac07b86
Dakolo14 <ajosedare4u@gmail.com>
2023-12-25 10:52:12 +0100

    major commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,3 +256,34 @@
   new PureCounter();
 
 })()
+
+
+
+
+/*=============== EMAIL JS ===============*/
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_gxnrze6','template_l2s6l9t','#contact-form','fma0b_FxxlVPQNcnk').then(() => {
+        //Show sent successfully
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+    }, () => {
+        // Show error message
+        contactMessage.textContent = ' Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail) 
